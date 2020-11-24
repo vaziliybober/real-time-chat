ca38d88ea7cb06c7c1e58c46e077404dbf1cfd6e
your username <vaziliybober@gmail.com>
2020-11-24 19:26:10 +0300

    copy backend

---
+++ webpack.config.babel.js
@@ -0,0 +1,45 @@
+// @ts-check
+
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const isProduction = process.env.NODE_ENV === 'production';
+console.log('isProduction', isProduction);
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  externals: {
+    gon: 'gon',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist/public`,
+    publicPath: '/assets/',
+  },
+  devServer: {
+    publicPath: '/assets/',
+    compress: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+};
