647d19ccc948e9daf41e8457b421f991aa76b270
vaziliybober <vaziliybober@gmail.com>
2021-01-14 21:12:41 +0300

    add scroll to bottom effect dependency in MessageBox

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,16 +1,18 @@
 import React, { useRef, useEffect } from 'react';
 
+import useChannels from '../hooks/useChannels.js';
 import useMessages from '../hooks/useMessages.js';
 
 const MessageBox = (props) => {
   const { children } = props;
   const [messages] = useMessages();
+  const [{ currentChannelId }] = useChannels();
 
   const MessageBoxRef = useRef();
   useEffect(() => {
     const div = MessageBoxRef.current;
     div.scrollTop = div.scrollHeight - div.clientHeight;
-  });
+  }, [currentChannelId]);
 
   return (
     <div className="col h-100">
